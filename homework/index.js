33ac60714502e6a29959a63e9381c630f9d892f6
Braedon <braedongough@gmail.com>
2021-11-28 13:07:40 +0100

    exercise 1

---
+++ homework/index.js
@@ -0,0 +1,45 @@
+const spiritAnimals = [
+  'strong wolf',
+  'crying butterfly',
+  'cunning fox',
+  'sensitive shellfish',
+  'smart monkey',
+]
+
+function getRandomInt() {
+  return Math.floor(Math.random() * spiritAnimals.length)
+}
+
+function generateSpiritAnimal() {
+  const value = document.querySelector('input').value
+
+  if (!value.trim()) {
+    document.querySelector('input').value = ''
+    return
+  }
+
+  const fullSpiritName = `${value} - ${spiritAnimals[getRandomInt()]}`
+
+  const spritAnimalMarkup = document.getElementById('spirit-animal')
+
+  spritAnimalMarkup.innerHTML = fullSpiritName
+
+  document.querySelector('input').value = ''
+}
+
+const button = document.querySelector('button')
+const select = document.querySelector('select')
+let currentSelectOption = select.options[select.options.selectedIndex].value
+
+select.addEventListener('change', function () {
+  console.log(currentSelectOption)
+  button.removeEventListener(currentSelectOption, () => {})
+
+  const nextSelectedOption = select.options[select.options.selectedIndex].value
+
+  button.addEventListener(nextSelectedOption, generateSpiritAnimal)
+
+  currentSelectOption = nextSelectedOption
+})
+
+button.addEventListener('click', generateSpiritAnimal)
